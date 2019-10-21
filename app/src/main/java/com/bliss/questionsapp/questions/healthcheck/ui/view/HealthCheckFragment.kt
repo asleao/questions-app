bbc40d4e9b228557cbfd7182e84a3251390ba0ef
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-10-21 12:12:21 -0300

    Adding base layout to deal with connection problems.

--- app/src/main/java/com/bliss/questionsapp/questions/healthcheck/ui/view/HealthCheckFragment.kt
+++ app/src/main/java/com/bliss/questionsapp/questions/healthcheck/ui/view/HealthCheckFragment.kt
@@ -6,29 +6,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.bliss.questionsapp.R
+import com.bliss.questionsapp.databinding.HealthCheckFragmentBinding
 import com.bliss.questionsapp.questions.healthcheck.viewmodel.HealthCheckViewModel
 
 class HealthCheckFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HealthCheckFragment()
-    }
-
     private lateinit var viewModel: HealthCheckViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(HealthCheckViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.health_check_fragment, container, false)
+    ): View {
+        val binding = DataBindingUtil.inflate<HealthCheckFragmentBinding>(
+            inflater,
+            R.layout.health_check_fragment,
+            container,
+            false
+        )
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(HealthCheckViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/bliss/questionsapp/questions/healthcheck/viewmodel/HealthCheckViewModel.kt
+++ app/src/main/java/com/bliss/questionsapp/questions/healthcheck/viewmodel/HealthCheckViewModel.kt
@@ -1,7 +1,12 @@
 package com.bliss.questionsapp.questions.healthcheck.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HealthCheckViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val loading = MutableLiveData<Boolean>()
+
+    init {
+        loading.value = true
+    }
 }
