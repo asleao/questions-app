61d9262b07052da39cd4203bd3bf579a40e83b84
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-10-21 23:44:53 -0300

    Creating ChoicesAdapter.

---
+++ app/src/main/java/com/bliss/questionsapp/questions/details/ui/adapters/ChoicesAdapter.kt
@@ -0,0 +1,37 @@
+package com.bliss.questionsapp.questions.details.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bliss.questionsapp.databinding.ChoicesItemBinding
+import com.bliss.questionsapp.questions.commons.model.Choice
+
+class ChoicesAdapter(
+    private val choices: List<Choice>,
+    private val choiceAction: (Choice) -> Unit
+) : RecyclerView.Adapter<ChoicesAdapter.ChoicesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChoicesViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ChoicesItemBinding.inflate(layoutInflater, parent, false)
+        return ChoicesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return choices.size
+    }
+
+    override fun onBindViewHolder(holder: ChoicesViewHolder, position: Int) {
+        holder.bind(choices[position], choiceAction)
+    }
+
+    class ChoicesViewHolder(private val binding: ChoicesItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(choice: Choice, choiceAction: (Choice) -> Unit) {
+            binding.choice = choice
+            binding.ivThumbsup.setOnClickListener {
+                choiceAction.invoke(choice)
+            }
+        }
+    }
+}
--- app/src/main/java/com/bliss/questionsapp/questions/details/ui/view/QuestionDetailFragment.kt
+++ app/src/main/java/com/bliss/questionsapp/questions/details/ui/view/QuestionDetailFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bliss.questionsapp.R
 import com.bliss.questionsapp.databinding.QuestionDetailFragmentBinding
+import com.bliss.questionsapp.questions.details.ui.adapters.ChoicesAdapter
 import com.bliss.questionsapp.questions.details.viewmodel.QuestionDetailViewModel
 import org.koin.android.ext.android.getKoin
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -19,6 +24,7 @@ class QuestionDetailFragment : Fragment() {
 
     private val viewModel: QuestionDetailViewModel by viewModel()
     private val args by navArgs<QuestionDetailFragmentArgs>()
+    private lateinit var binding: QuestionDetailFragmentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +33,15 @@ class QuestionDetailFragment : Fragment() {
     }
 
     private fun setupObservers() {
+        viewModel.question.observe(this, Observer { question ->
+            binding.rvChoices.adapter = ChoicesAdapter(question.choices) { choice ->
+                Toast.makeText(requireContext(), choice.title, Toast.LENGTH_LONG).show()
+            }
+        })
+        setupErrorObserver()
+    }
+
+    private fun setupErrorObserver() {
         viewModel.error.observe(this, Observer { error ->
             viewModel.showLoading(false)
             viewModel.hasConnectionProblems(true)
@@ -39,7 +54,7 @@ class QuestionDetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<QuestionDetailFragmentBinding>(
+        binding = DataBindingUtil.inflate(
             inflater,
             R.layout.question_detail_fragment,
             container,
@@ -48,7 +63,18 @@ class QuestionDetailFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         setupTryAgainClickListener(binding.iBaseLayout.btTryAgain)
+        setupRecyclerView(binding.rvChoices)
         return binding.root
+    }
+
+    private fun setupRecyclerView(rvChoices: RecyclerView) {
+        val layoutManager = LinearLayoutManager(requireContext())
+        val divider = DividerItemDecoration(
+            rvChoices.context,
+            layoutManager.orientation
+        )
+        rvChoices.layoutManager = layoutManager
+        rvChoices.addItemDecoration(divider)
     }
 
     private fun setupTryAgainClickListener(btTryAgain: Button) {
