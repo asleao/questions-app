3ccbf28dfa52292a423e7e3c79d9cbf1b1e1ca4c
Andre Spadetto Leao <andre.sp.leao@gmail.com>
2019-10-22 02:45:22 -0300

    Implementing the layout for share screen.

--- app/src/main/java/com/bliss/questionsapp/questions/share/viewmodel/QuestionShareViewModel.kt
+++ app/src/main/java/com/bliss/questionsapp/questions/share/viewmodel/QuestionShareViewModel.kt
@@ -1,5 +1,6 @@
 package com.bliss.questionsapp.questions.share.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import com.bliss.questionsapp.core.base.BaseViewModel
 import com.bliss.questionsapp.questions.commons.data.QuestionRepository
 
@@ -7,6 +8,9 @@ class QuestionShareViewModel(
     private val questionRepository: QuestionRepository,
     private val questionId: Int
 ) : BaseViewModel(){
+
+    val email = MutableLiveData<String>()
+    val errorEnabled = MutableLiveData<Boolean>()
 
     override fun tryAgain() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
